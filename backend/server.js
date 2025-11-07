e57c4b8ecb29943843776fb072fadc96b551b26b
Rajib203 <skrajibuddin472@gmail.com>
2025-11-07 21:33:22 +0530

    final front url in backend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT=4000
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://resume-builder-frontend1.onrender.com',
   credentials: true,
 }));
 connectDB();
